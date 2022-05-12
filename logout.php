140f81dae66fe487972ec9844cba60b0b73105b6
tifa90 <latifa.akpo@yahoo.fr>
2022-05-12 22:48:45 +0200

    Delete logout.php

--- logout.php
+++
@@ -1,8 +0,0 @@
-<?php    
-// Fichier de déconnexion
-    
-session_start();  
-//session_destroy sert à detruire la session  
-session_destroy();  
-header("Location: ../vue/connexion.php");   
-?> 
